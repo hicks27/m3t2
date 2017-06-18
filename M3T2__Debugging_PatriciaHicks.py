80d0051f27b5ed6840099421a59bee89a4f15fd8
hicks27 <hicksp2597@student.faytechcc.edu>
2017-06-17 20:34:55 -0700

    Add files via upload
    M3T2 completed

---
+++ M3T2__Debugging_PatriciaHicks.py
@@ -0,0 +1,33 @@
+#Roman Numerals.
+#6/17/17.
+#CTI-110 M3T2-Debugging
+#Patricia Hicks
+#
+usernumber = int( input( "Please enter a number:" ) )
+print()
+if usernumber == 1:
+    print( "I" )
+elif usernumber == 2:
+    print( "II" )
+elif usernumber == 3:
+    print( "III" )
+elif usernumber == 4:
+    print( "IV" )
+elif usernumber == 5:
+    print( "V" )
+elif usernumber == 6:
+    print( "VI" )
+elif usernumber == 7:
+    print( "VII" )
+elif usernumber == 8:
+    print( "VIII" )
+elif usernumber == 9:
+    print( "IX" )
+elif usernumber == 10:
+    print( "X" )
+else:
+    print( "Error: Please restart the program and enter a number between" + \
+          "1 and 10." )
+
+    
+        
